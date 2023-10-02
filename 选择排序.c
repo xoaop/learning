3313116621917e263099798237b951afcaf4f5f2
xoaop <138418820+xoaop@users.noreply.github.com>
2023-10-02 23:54:52 +0800

    Update 选择排序.c

--- 选择排序.c
+++ 选择排序.c
@@ -1,1 +1,31 @@
+void selection(int *nums, int numsSize) 
+{
+	
+  for (int i = 0; i < numsSize; i++) 
+  {
+		int minj = i;
 
+		int min = INT_MAX;//记得调用<limits.h>
+
+		for (int j = i; j < numsSize; j++) 
+    {
+			if (nums[j] < min) {
+				min = nums[j];
+				minj = j;
+			}
+
+		}
+		if (minj != i) 
+    {
+			int tmp = nums[i];
+			nums[i] = nums[minj];
+			nums[minj] = tmp;
+		}
+	
+  }
+
+  
+}
+
+
+  
